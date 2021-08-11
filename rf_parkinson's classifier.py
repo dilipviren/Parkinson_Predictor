66f6fa755cc2dcedc6ef4254ef23c230e50c310a
dilipviren <87564983+dilipviren@users.noreply.github.com>
2021-08-11 15:53:29 +0530

    Update rf_parkinson's classifier.py

--- rf_parkinson's classifier.py
+++ rf_parkinson's classifier.py
@@ -1,80 +1,41 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[50]:
-
-
 import numpy as np 
 import pandas as pd 
 import os, sys
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
-
-# In[51]:
-
-
 df=pd.read_csv('parkinsons.data')
 df.head()
-
-
-# In[52]:
-
 
 df.columns
 
 
-# In[53]:
-
-
 data=df.loc[:,df.columns!='status'].values[:,1:]
 labels=df.loc[:,df.columns=='status'].values
-
-
-# In[54]:
-
 
 scaler=MinMaxScaler()
 x=scaler.fit_transform(data)
 y=labels
 
 
-# In[55]:
-
-
 from sklearn.ensemble import RandomForestClassifier
+
 rf=RandomForestClassifier(n_estimators=150)
 xtrain,xtest,ytrain,ytest=train_test_split(x,y,test_size=0.20,random_state=100)
 
-
-# In[47]:
-
-
 rf.fit(xtrain,ytrain)
-
-
-# In[48]:
-
 
 predictions=rf.predict(xtest)
 
 
-# In[49]:
-
-
 from sklearn.metrics import accuracy_score
+
 print(accuracy_score(predictions,ytest))
 
 
-# In[57]:
-
-
 from sklearn.model_selection import GridSearchCV
+
 print(rf.get_params())
-
-
-# In[71]:
-
 
 n_estimators = [int(x) for x in np.linspace(start=200,stop=2000,num=5)]
 max_features = ['auto']
@@ -93,32 +54,11 @@ random_grid = {'n_estimators': n_estimators,
 print(random_grid)
 
 
-# In[72]:
-
-
 grid=GridSearchCV(RandomForestClassifier(),random_grid,refit=True,verbose=4,cv=3)
 
-
-# In[73]:
-
-
 grid.fit(xtrain,ytrain)
-
-
-# In[74]:
-
-
 grid.best_estimator_
-
-
-# In[75]:
-
-
 grid.best_params_
-
-
-# In[79]:
-
 
 best_model=RandomForestClassifier(bootstrap=True, ccp_alpha=0.0, class_weight=None,
                        criterion='gini', max_depth=85, max_features='auto',
@@ -128,16 +68,16 @@ best_model=RandomForestClassifier(bootstrap=True, ccp_alpha=0.0, class_weight=No
                        min_weight_fraction_leaf=0.0, n_estimators=200,
                        n_jobs=None, oob_score=False, random_state=None,
                        verbose=0, warm_start=False)
+
+
 best_model.fit(xtrain,ytrain)
 b_predictions=best_model.predict(xtest)
+
+# measuring results
+
 print(accuracy_score(b_predictions,ytest))
 
-
-# In[80]:
-
-
-# best model has the same accuracy as the original model 
-# more extensive parameters required in the search grid
+# saving the model
 
 import pickle 
 pickle.dump(best_model,open('rf_model_v1','wb'))
